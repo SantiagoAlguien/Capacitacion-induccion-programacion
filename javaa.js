5de1aa87b13a9f449b55c24137f95f62a0369cc3
SantiagoAlguien <28.snll.05@gmail.com>
2023-02-23 17:54:42 -0500

    lol

--- javaa.js
+++ javaa.js
@@ -1,28 +1,70 @@
-alert("holaaaaaaaa")
+let ataquejugador
+let ataqueenemigo
 
 function IniciarJuego(){
     let botonMascotaJugador = document.getElementById('Selecionar-Mascota')
     botonMascotaJugador.addEventListener('click',selecionarMascotaJugador)
+
+    let BotonFuego=document.getElementById('boton-fuego')
+    BotonFuego.addEventListener('click', ataqueFuego)
+    
+    let BotonAgua=document.getElementById('boton-agua')
+    BotonAgua.addEventListener('click', ataqueAgua)
+    
+    let BotonTierra=document.getElementById('boton-tierra')
+    BotonTierra.addEventListener('click', ataqueTierra)
 }
 
+function ataqueFuego(){
+    ataquejugador='FUEGO'
+    alert(ataquejugador)
+}
+function ataqueAgua(){
+    ataqueJugador='AGUA'
+    alert(ataquejugador)
+}
+function ataqueTierra(){
+    ataquejugador='AGUA'
+    alert(ataquejugador)
+}
+function ataqueenemigoaleatorio(){
+    let ataqueenemigoaleatorio= aleatorio(1,3)
+    if(ataqueenemigoaleatorio== 1){ataqueenemigo='AGUA'}
+    else if (ataqueenemigoaleatorio== 2){ataqueenemigo='FUEGO'}
+    else if (ataqueenemigoaleatorio== 3){ataqueenemigo='TIERRA'}
+}
 
-let NombreBicho= 0
 function selecionarMascotaJugador(){
     let imputHipodoge = document.getElementById('Hipodoge')
     let imputcapipepo = document.getElementById('Capipepo')
     let impuratatopo = document.getElementById('ratatopo')
-    let impuseleciona = document.getElementById
-    if(imputHipodoge.checked) {
-        alert('Selecionaste Hipodoge')
+    let spanJugador = document.getElementById('Mascota-jugador')
+
+
+    if(imputHipodoge.checked){
+     spanJugador.innerHTML='Hipodoge'
     } else if (imputcapipepo.checked){
-        alert('Selecionate Capipepo')
+        spanJugador.innerHTML='Capipepo'
     } else if (impuratatopo.checked){
-        alert('Selecionate ratatopo')
+        spanJugador.innerHTML='Ratatopo'
     } else {
         alert('seleciona un bicho')
     }
+    eleccionarMascotaEnemigo()
 }
+function eleccionarMascotaEnemigo(){
+let mascotaaleatoria = aleatorio(1,3) 
+let spanEnemigo = document.getElementById('Mascota-enemigo')
+if(mascotaaleatoria== 1){
+    spanEnemigo.innerHTML='Hipodoge'
+}else if(mascotaaleatoria==2){
+    spanEnemigo.innerHTML='Capipepo'
+}else if(mascotaaleatoria==3){
+    spanEnemigo.innerHTML='Ratatopo'
+}}
 
+function aleatorio(min,max){
+return Math.floor(Math.random() * (max-min+1)+ min )}
 
 window.addEventListener("load",IniciarJuego)
 
