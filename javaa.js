adf680ca71177bae0bf1e3ecb22b6f08877832f7
SantiagoAlguien <28.snll.05@gmail.co>
2023-02-24 22:10:31 -0500

    a

--- javaa.js
+++ javaa.js
@@ -24,7 +24,7 @@ function ataqueAgua(){
     alert(ataquejugador)
 }
 function ataqueTierra(){
-    ataquejugador='Fuego'
+    ataquejugador='TIERRA'
     alert(ataquejugador)
 }
 function ataqueenemigoaleatorio(){
